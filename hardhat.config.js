ca65ebf404608f4a89b84a67acdf420bd3e64df4
CHI-NONSO1 <chinonso_patrick@yahoo.com>
2022-12-18 02:47:40 +0100

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,41 @@
+require("@nomicfoundation/hardhat-toolbox");
+
+require("@openzeppelin/hardhat-upgrades");
+
+require("dotenv").config();
+
+// The next line is part of the sample project, you don't need it in your
+// project. It imports a Hardhat task definition, that can be used for
+// testing the frontend.
+require("./tasks/faucet");
+
+//====================================================================
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337, // We set 1337 to make interacting with MetaMask simpler
+    },
+  },
+};
+
+//=================================================================
+
+// Replace this private key with your Goerli account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
